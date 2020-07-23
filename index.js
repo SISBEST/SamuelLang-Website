ff3a6983d9e047886213abd056ce766e101ed65a
Samuel Sharp <samuelsharp4@gmail.com>
2020-07-23 14:02:05 -0700

    Add structure

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.get('/', (req, res) => {
 	res.render('home');
 });
 
+app.get('/get', (req, res) => {
+	res.render('get');
+});
+
+app.get('/repl', (req, res) => {
+	res.render('repl');
+});
+
+app.get('/contribute', (req, res) => {
+	res.render('contribute');
+});
+
+app.get('/packages', (req, res) => {
+	res.render('packages/index');
+});
+
+app.get('/packages/:name', (req, res) => {
+	res.render('packages/info', {
+		name: req.params.name
+	});
+});
+
 app.get('/docs', (req, res) => {
 	res.render('docs/index');
 });
